3090456be5e98a8873f74f7ae7b972d7a9e63c30
Robert Va?an <Robert Va?an@localhost>
2014-11-28 18:59:18 +0100

    Sample control library and an app

---
+++ FacadeSample/MainModel.cs
@@ -0,0 +1,16 @@
+﻿using Assisticant.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacadeSample
+{
+    class MainModel
+    {
+        public readonly Observable<string> FirstName = new Observable<string>("Robert");
+        public readonly Observable<string> LastName = new Observable<string>("Važan");
+        public readonly Observable<bool> IsReversed = new Observable<bool>();
+    }
+}
---
+++ SampleControlLibrary/FullName.cs
@@ -0,0 +1,48 @@
+﻿using Assisticant.Facades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace SampleControlLibrary
+{
+    public class FullName : Control
+    {
+        readonly FacadeInstance FacadeInstance;
+
+        public static readonly DependencyProperty FirstProperty = DependencyProperty.Register("First", typeof(string), typeof(FullName));
+        public string First { get { return (string)GetValue(FirstProperty); } set { SetValue(FirstProperty, value); } }
+
+        public static readonly DependencyProperty LastProperty = DependencyProperty.Register("Last", typeof(string), typeof(FullName));
+        public string Last { get { return (string)GetValue(LastProperty); } set { SetValue(LastProperty, value); } }
+
+        public static readonly DependencyProperty IsReversedProperty = DependencyProperty.Register("IsReversed", typeof(bool), typeof(FullName));
+        public bool IsReversed { get { return (bool)GetValue(IsReversedProperty); } set { SetValue(IsReversedProperty, value); } }
+
+        static FullName()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FullName), new FrameworkPropertyMetadata(typeof(FullName)));
+        }
+
+        public FullName()
+        {
+            FacadeInstance = new FacadeInstance(this, typeof(FullNameModel));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            FacadeInstance.ApplyModel(GetTemplateChild("Root"));
+            base.OnApplyTemplate();
+        }
+
+        protected override void OnPropertyChanged(DependencyPropertyChangedEventArgs args)
+        {
+            base.OnPropertyChanged(args);
+            if (FacadeInstance != null)
+                FacadeInstance.UpdateModel(args);
+        }
+    }
+}
